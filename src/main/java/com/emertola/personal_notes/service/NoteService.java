d2e1f4788e511c009b1a3726d9b9f0acff027fab
emertola <emertola@gmail.com>
2024-11-16 10:31:49 +0800

    chore: note service interface

--- src/main/java/com/emertola/personal_notes/service/NoteService.java
+++ src/main/java/com/emertola/personal_notes/service/NoteService.java
@@ -1,31 +1,12 @@
 package com.emertola.personal_notes.service;
 
 import com.emertola.personal_notes.model.Note;
-import com.emertola.personal_notes.repository.NoteRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class NoteService {
-    private final NoteRepository noteRepository;
-
-    public NoteService(NoteRepository noteRepository) {
-        this.noteRepository = noteRepository;
-    }
-
-    public List<Note> getAllNotes() {
-        return noteRepository.findAll();
-    }
-
-    public Optional<Note> getNoteById(Long id) {
-        return noteRepository.findById(id);
-    }
-
-    @Transactional
-    public Note createNote(Note note) {
-        return noteRepository.save(note);
-    }
+public interface NoteService {
+    List<Note> getAllNotes();
+    Optional<Note> getNoteById(Long id);
+    Note createNote(Note note);
 }
---
+++ src/main/java/com/emertola/personal_notes/service/impl/NoteServiceImpl.java
@@ -0,0 +1,33 @@
+package com.emertola.personal_notes.service.impl;
+
+import com.emertola.personal_notes.model.Note;
+import com.emertola.personal_notes.repository.NoteRepository;
+import com.emertola.personal_notes.service.NoteService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NoteServiceImpl implements NoteService {
+    private final NoteRepository noteRepository;
+
+    public NoteServiceImpl(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @Override
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    @Override
+    public Note createNote(Note note) {
+        return noteRepository.save(note);
+    }
+
+    @Override
+    public Optional<Note> getNoteById(Long id) {
+        return noteRepository.findById(id);
+    }
+}
